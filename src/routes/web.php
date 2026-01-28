fa5737f5e1fccb3f72108f401c4c575d5e2bc648
yoshinobu k <ysnb5884@gmail.com>
2026-01-29 03:41:20 +0900

    ミドルウェア設定（認証必要なルートにauthミドルウェア追加）

--- src/routes/web.php
+++ src/routes/web.php
@@ -24,35 +24,38 @@ Route::get('/', [ProductController::class, 'index']);
 // 商品詳細
 Route::get('/item/{id}', [ProductController::class, 'show']);
 
-// マイページ
-Route::get('/mypage', [MypageController::class, 'index']);
+// 認証必要（グループ化）
+Route::middleware('auth')->group(function () {
+    // マイページ
+    Route::get('/mypage', [MypageController::class, 'index']);
 
-// プロフィール編集画面
-Route::get('/mypage/profile', [MypageController::class, 'edit']);
+    // プロフィール編集画面
+    Route::get('/mypage/profile', [MypageController::class, 'edit']);
 
-// プロフィール更新
-Route::post('/mypage/profile', [MypageController::class, 'update']);
+    // プロフィール更新
+    Route::post('/mypage/profile', [MypageController::class, 'update']);
 
-//いいね
-Route::post('/item/{id}/like', [LikeController::class, 'toggle']);
+    //いいね
+    Route::post('/item/{id}/like', [LikeController::class, 'toggle']);
 
-// 購入画面の表示
-Route::get('/purchase/{id}', [OrderController::class, 'create']);
+    // 購入画面の表示
+    Route::get('/purchase/{id}', [OrderController::class, 'create']);
 
-//送付先住所入力画面
-Route::get('/purchase/address/{id}', [OrderController::class, 'edit']);
+    //送付先住所入力画面
+    Route::get('/purchase/address/{id}', [OrderController::class, 'edit']);
 
-//送付先住所更新
-Route::post('/purchase/address/{id}', [OrderController::class, 'update']);
+    //送付先住所更新
+    Route::post('/purchase/address/{id}', [OrderController::class, 'update']);
 
-//購入処理
-Route::post('/purchase/{id}', [OrderController::class, 'store']);
+    //購入処理
+    Route::post('/purchase/{id}', [OrderController::class, 'store']);
 
-//コメント投稿
-Route::post('/item/{id}/comment', [CommentController::class, 'store']);
+    //コメント投稿
+    Route::post('/item/{id}/comment', [CommentController::class, 'store']);
 
-//出品情報入力画面の表示
-Route::get('/sell', [ProductController::class, 'create'])->name('products.create');
+    //出品情報入力画面の表示
+    Route::get('/sell', [ProductController::class, 'create'])->name('products.create');
 
-//出品処理
-Route::post('/sell', [ProductController::class, 'store'])->name('products.store');
+    //出品処理
+    Route::post('/sell', [ProductController::class, 'store'])->name('products.store');
+});
