18f73614ccc3599c665d7436cfde689d1d8ba04e
yoshinobu k <ysnb5884@gmail.com>
2026-02-08 03:40:04 +0900

    コメント表示レイアウト修正完了（README・テスト実装前の最終コミット）

--- src/resources/views/products/show.blade.php
+++ src/resources/views/products/show.blade.php
@@ -154,6 +154,13 @@
             flex-shrink: 0;
         }
 
+        .comment-avatar img {
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+            border-radius: 50%;
+        }
+
         .comment-content {
             flex: 1;
         }
